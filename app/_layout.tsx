e44cae2b41bbc03e93ba84b04e3fceca0172e28b
heawer <accounts+ghtest@sapphic.moe>
2026-04-14 08:57:34 +0500

    fix warning

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,42 +1,49 @@
-import {Stack} from "expo-router";
-import {ThemeProvider, DarkTheme, DefaultTheme} from "@react-navigation/native";
-import {useColorScheme} from "react-native";
-import "../global.css";
+import {
+    DarkTheme,
+    DefaultTheme,
+    ThemeProvider,
+} from "@react-navigation/native";
+import { Stack } from "expo-router";
+import { useColorScheme } from "react-native";
 import { ParentDataProvider } from "../contexts/ParentDataContext";
+import "../global.css";
 
 export default function RootLayout() {
-    const colorScheme = useColorScheme();
+  const colorScheme = useColorScheme();
 
-    return (
-        <ParentDataProvider>
-        <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-            <Stack
-                screenOptions={{
-                    headerShown: false,
-                    animation: "fade",
-                }}
-            >
-                {/* AUTH + INTRO (показываются первыми) */}
-                <Stack.Screen name="(auth)"/>
+  return (
+    <ParentDataProvider>
+      <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+        <Stack
+          screenOptions={{
+            headerShown: false,
+            animation: "fade",
+          }}
+        >
+          {/* AUTH + INTRO (показываются первыми) */}
+          <Stack.Screen name="(auth)" />
 
-                {/* PROFILE CREATION (идут после выбора роли) */}
-                <Stack.Screen name="profile"/>
+          {/* PROFILE CREATION (идут после выбора роли) */}
+          <Stack.Screen name="profile" />
 
-                {/* MAIN TABS (доступно только после входа) */}
-                <Stack.Screen name="(tabs)"/>
+          {/* MAIN TABS (доступно только после входа) */}
+          <Stack.Screen name="(tabs)" />
 
-                {/* если вдруг понадобится модальное окно */}
-                {/* <Stack.Screen name="modal" options={{presentation: "modal"}}/> */}
+          {/* если вдруг понадобится модальное окно */}
+          {/* <Stack.Screen name="modal" options={{presentation: "modal"}}/> */}
 
-                {/* <Stack.Screen
+          {/* <Stack.Screen
                     name="modal/course"
                     options={{presentation: "modal"}}
                 /> */}
 
-                {/* TEST ROUTE */}
-                <Stack.Screen name="test" options={{presentation: "fullScreenModal"}}/>
-            </Stack>
-        </ThemeProvider>
-        </ParentDataProvider>
-    );
+          {/* TEST ROUTE */}
+          <Stack.Screen
+            name="test/index"
+            options={{ presentation: "fullScreenModal" }}
+          />
+        </Stack>
+      </ThemeProvider>
+    </ParentDataProvider>
+  );
 }
